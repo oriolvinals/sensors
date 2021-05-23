c411fcbda6c842035b4c2f763a64394fc2a03b97
Oriol <oriolvinals@hotmial.com>
2021-05-23 03:07:26 +0200

    fix gps

--- src/pages/Gps.tsx
+++ src/pages/Gps.tsx
@@ -9,7 +9,21 @@ import {
 } from "@ionic/react";
 import { Geolocation } from "@capacitor/geolocation";
 import { useEffect, useState } from "react";
+import Leaflet from "leaflet";
 import { Map, Marker, TileLayer, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import L from "leaflet";
+
+let DefaultIcon = L.icon({
+	...L.Icon.Default.prototype.options,
+	iconUrl: icon,
+	iconRetinaUrl: iconRetina,
+	shadowUrl: iconShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Gps = () => {
 	const [gpsInfo, setGpsInfo] = useState<any>();
@@ -42,22 +56,22 @@ const Gps = () => {
 				</IonHeader>
 				{gpsInfo && (
 					<>
-						<div className="p-4 w-full bg-black z-50">
+						<div className="p-4 w-full bg-black">
 							<p>Latitude: {gpsInfo.coords.latitude}</p>
 							<p>Longitude: {gpsInfo.coords.longitude}</p>
 						</div>
 						<div>
 							<Map
-								className="z-30"
 								center={{
 									lat: gpsInfo.coords.latitude,
 									lng: gpsInfo.coords.longitude,
 								}}
 								zoom={15}
 								scrollWheelZoom={false}
+								className="w-screen h-96"
 							>
 								<TileLayer
-									attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+									attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 									url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 								/>
 								<Marker
